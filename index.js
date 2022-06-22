8a634920af89bc5d87a364bd05d9b8248d65828e
om7271prakash <om7271prakash@gmail.com>
2022-06-22 16:14:30 +0530

    Logging data using middleware

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ const friends = [
         id: 1,
         name: "Rakesh Sharma"
     }
-]
+];
+
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
 
 app.get('', (req, res) => {
     res.send("Hello there! This is simple text");
@@ -41,7 +48,7 @@ app.get('/friends/:friendId', (req, res) => {
     const friend = friends[friendId];
     console.log(friend)
     if(friend){
-        res.status(200).json(friend);
+        res.status(200).json(friend);   
     }else{
         res.status(404).json({
             error: "Friend does not exist."
